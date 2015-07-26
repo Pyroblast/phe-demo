c8f6d702e621914c6b37198cca1bc497d3ef75bb
Pyroblast <robertyi@126.com>
2015-07-26 15:01:06 +0800

    计算流程完成

--- submit.php
+++ submit.php
@@ -124,7 +124,7 @@ $W_2 = $Q/$C_p2/$COCI;
 //echo "$W_1" . "<br >" . "$W_2" . "<br >";
 //根据输入的温度和厚度查库找到合适的材质的导热率k_p，
 //【GB/151中规定，100°C下的不锈钢导热率为16.3，可以暂定这个值】
-$k_p = 16.3;
+$k_p = 16;
 /*
 Default：流程Np=1【目前我们只计算单流程】，通过流量计算管口流速Gp
 【这里加入一个约束，初步设定Gp属于1-5m/s区间内的值为合适口径，返回对应的口径，
@@ -175,20 +175,7 @@ $plate = array(
 	'TL_m_Eu' => -0.210683492,
 );
 //print_r($plate);
-//计算T_1_w、T_2_w，算法待补充。
-$T_1_w = round(62.24);
-$T_2_w = round(59.76079105);
-//根据T_1_w和T_2_w的值查询粘度。
-//T_1_w
-$sql3 = "SELECT * FROM `water_properties` WHERE `T`= '" . $T_1_w . "'";
-$rs3 = $db->query($sql3);
-$row3 = $rs3->fetch();
-$miu_d_T_1_w = $row3[6];
-//T_2_w
-$sql4 = "SELECT * FROM `water_properties` WHERE `T`= '" . $T_2_w . "'";
-$rs4 = $db->query($sql4);
-$row4 = $rs4->fetch();
-$miu_d_T_2_w = $row4[6];
+
 //计算TK
 if ($W_1 >= $W_2) {
 	
@@ -199,12 +186,49 @@ if ($W_1 >= $W_2) {
 	$TK_V_1_c=$TK_V_2_c * $roll2 * $W_1/$W_2/$roll1;
 	$TK_Re_2=$TK_V_2_c*$roll2*$plate['De']/$miu_d2;
 	$TK_Re_1=$TK_V_1_c*$roll1*$plate['De']/$miu_d1;
-	$TK_Nu_2=$plate['TK_C_Nu']*exp($plate['TK_m_Nu']*log($TK_Re_2))
-	*exp(0.4*log($Pr2))*exp(0.14*log(($miu_d2/$miu_d_T_2_w)));
-	$TK_Nu_1=$plate['TK_C_Nu']*exp($plate['TK_m_Nu']*log($TK_Re_1))
-	*exp(0.4*log($Pr1))*exp(0.14*log(($miu_d1/$miu_d_T_1_w)));
-	$TK_h_2=$TK_Nu_2*$k2/$plate['De'];
-	$TK_h_1=$TK_Nu_1*$k1/$plate['De'];
+	$TK_T_1_w = 0.5*($T_2_b + $T_1_b);
+	$i = 0;
+
+
+	$start=$T_2_b;
+	$end = $T_1_b;
+	do {
+		//查询粘度
+		$sql3 = "SELECT * FROM `water_properties` WHERE `T`= '" . round($TK_T_1_w) . "'";
+		$rs3 = $db->query($sql3);
+		$row3 = $rs3->fetch();
+		$TK_miu_d_T_1_w = $row3[6];
+
+		$TK_Nu_1=$plate['TK_C_Nu']*exp($plate['TK_m_Nu']*log($TK_Re_1))
+			*exp(0.33*log($Pr1))*exp(0.14*log(($miu_d1/$TK_miu_d_T_1_w)));
+
+		
+		$TK_h_1=$TK_Nu_1*$k1/$plate['De'];
+		$TK_Q_1 = $TK_h_1 * ($T_1_b - $TK_T_1_w);
+		$TK_T_2_w = $TK_T_1_w - $TK_Q_1 * $plate['t_p']/$k_p;
+
+		$sql4 = "SELECT * FROM `water_properties` WHERE `T`= '" . round($TK_T_2_w) . "'";
+		$rs4 = $db->query($sql4);
+		$row4 = $rs4->fetch();
+		$TK_miu_d_T_2_w = $row4[6];
+
+		$TK_Nu_2=$plate['TK_C_Nu']*exp($plate['TK_m_Nu']*log($TK_Re_2))
+			*exp(0.4*log($Pr2))*exp(0.14*log(($miu_d2/$TK_miu_d_T_2_w)));
+		$TK_h_2=$TK_Nu_2*$k2/$plate['De'];
+		$TK_Q_2 = $TK_h_2 * ($TK_T_2_w - $T_2_b);
+
+
+		if ($TK_Q_1 >= $TK_Q_2) {
+			$start = $TK_T_1_w;
+		} else {
+			$end = $TK_T_1_w;
+		}
+		$TK_T_1_w = 0.5*($start + $end);
+
+		$i++;
+	} while (abs(($TK_Q_1-$TK_Q_2)/$TK_Q_1) > 0.001);
+
+
 	$TK_U=1/((1/$TK_h_2)+(1/$TK_h_1)+($plate['t_p']/$k_p));
 	$TK_NTU_1=2*$TK_U*$plate['A_r']/($TK_V_1_c*$roll1*$plate['A_c']*$C_p1*1000);
 	$TK_sigma_1=($TK_V_1_c*$roll1*$C_p1)/($TK_V_2_c*$roll2*$C_p2);
@@ -213,24 +237,7 @@ if ($W_1 >= $W_2) {
 	$TK_t_1=$TK_R_1*$Delta_ti;
 	$TK_W_1=$TK_V_1_c*$plate['A_c']*$roll1;
 }	
-/*
-echo "TK参数：<br >".
-"$roll2<br />".
-"$TK_V_2_c<br />".
-"$TK_V_1_c<br >".
-"$TK_Re_2<br >".
-"$TK_Re_1<br >".
-"$TK_Nu_2<br >".
-"$TK_Nu_1<br >";
-ec($TK_h_2);
-ec($TK_h_1);
-ec($TK_U);
-ec($TK_NTU_1);
-ec($TK_sigma_1);
-ec($TK_R_1);
-ec($TK_t_1);
-ec($TK_W_1);
-*/
+
 //计算TM
 if ($W_1 >= $W_2) {
 	
@@ -241,12 +248,49 @@ if ($W_1 >= $W_2) {
 	$TM_V_1_c=$TM_V_2_c * $roll2 * $W_1/$W_2/$roll1;
 	$TM_Re_2=$TM_V_2_c*$roll2*$plate['De']/$miu_d2;
 	$TM_Re_1=$TM_V_1_c*$roll1*$plate['De']/$miu_d1;
-	$TM_Nu_2=$plate['TM_C_Nu']*exp($plate['TM_m_Nu']*log($TM_Re_2))
-	*exp(0.4*log($Pr2))*exp(0.14*log(($miu_d2/$miu_d_T_2_w)));
-	$TM_Nu_1=$plate['TM_C_Nu']*exp($plate['TM_m_Nu']*log($TM_Re_1))
-	*exp(0.4*log($Pr1))*exp(0.14*log(($miu_d1/$miu_d_T_1_w)));
-	$TM_h_2=$TM_Nu_2*$k2/$plate['De'];
-	$TM_h_1=$TM_Nu_1*$k1/$plate['De'];
+	$TM_T_1_w = 0.5*($T_2_b + $T_1_b);
+	$i = 0;
+
+
+	$start=$T_2_b;
+	$end = $T_1_b;
+	do {
+		//查询粘度
+		$sql3 = "SELECT * FROM `water_properties` WHERE `T`= '" . round($TM_T_1_w) . "'";
+		$rs3 = $db->query($sql3);
+		$row3 = $rs3->fetch();
+		$TM_miu_d_T_1_w = $row3[6];
+
+		$TM_Nu_1=$plate['TM_C_Nu']*exp($plate['TM_m_Nu']*log($TM_Re_1))
+			*exp(0.33*log($Pr1))*exp(0.14*log(($miu_d1/$TM_miu_d_T_1_w)));
+
+		
+		$TM_h_1=$TM_Nu_1*$k1/$plate['De'];
+		$TM_Q_1 = $TM_h_1 * ($T_1_b - $TM_T_1_w);
+		$TM_T_2_w = $TM_T_1_w - $TM_Q_1 * $plate['t_p']/$k_p;
+
+		$sql4 = "SELECT * FROM `water_properties` WHERE `T`= '" . round($TM_T_2_w) . "'";
+		$rs4 = $db->query($sql4);
+		$row4 = $rs4->fetch();
+		$TM_miu_d_T_2_w = $row4[6];
+
+		$TM_Nu_2=$plate['TM_C_Nu']*exp($plate['TM_m_Nu']*log($TM_Re_2))
+			*exp(0.4*log($Pr2))*exp(0.14*log(($miu_d2/$TM_miu_d_T_2_w)));
+		$TM_h_2=$TM_Nu_2*$k2/$plate['De'];
+		$TM_Q_2 = $TM_h_2 * ($TM_T_2_w - $T_2_b);
+
+
+		if ($TM_Q_1 >= $TM_Q_2) {
+			$start = $TM_T_1_w;
+		} else {
+			$end = $TM_T_1_w;
+		}
+		$TM_T_1_w = 0.5*($start + $end);
+
+		$i++;
+	} while (abs(($TM_Q_1-$TM_Q_2)/$TM_Q_1) > 0.001);
+
+
 	$TM_U=1/((1/$TM_h_2)+(1/$TM_h_1)+($plate['t_p']/$k_p));
 	$TM_NTU_1=2*$TM_U*$plate['A_r']/($TM_V_1_c*$roll1*$plate['A_c']*$C_p1*1000);
 	$TM_sigma_1=($TM_V_1_c*$roll1*$C_p1)/($TM_V_2_c*$roll2*$C_p2);
@@ -255,24 +299,7 @@ if ($W_1 >= $W_2) {
 	$TM_t_1=$TM_R_1*$Delta_ti;
 	$TM_W_1=$TM_V_1_c*$plate['A_c']*$roll1;
 }	
-/*
-echo "TM参数：<br >".
-"$roll2<br />".
-"$TM_V_2_c<br />".
-"$TM_V_1_c<br >".
-"$TM_Re_2<br >".
-"$TM_Re_1<br >".
-"$TM_Nu_2<br >".
-"$TM_Nu_1<br >";
-ec($TM_h_2);
-ec($TM_h_1);
-ec($TM_U);
-ec($TM_NTU_1);
-ec($TM_sigma_1);
-ec($TM_R_1);
-ec($TM_t_1);
-ec($TM_W_1);
-*/
+
 //计算TL
 if ($W_1 >= $W_2) {
 	
@@ -283,12 +310,49 @@ if ($W_1 >= $W_2) {
 	$TL_V_1_c=$TL_V_2_c * $roll2 * $W_1/$W_2/$roll1;
 	$TL_Re_2=$TL_V_2_c*$roll2*$plate['De']/$miu_d2;
 	$TL_Re_1=$TL_V_1_c*$roll1*$plate['De']/$miu_d1;
-	$TL_Nu_2=$plate['TL_C_Nu']*exp($plate['TL_m_Nu']*log($TL_Re_2))
-	*exp(0.4*log($Pr2))*exp(0.14*log(($miu_d2/$miu_d_T_2_w)));
-	$TL_Nu_1=$plate['TL_C_Nu']*exp($plate['TL_m_Nu']*log($TL_Re_1))
-	*exp(0.4*log($Pr1))*exp(0.14*log(($miu_d1/$miu_d_T_1_w)));
-	$TL_h_2=$TL_Nu_2*$k2/$plate['De'];
-	$TL_h_1=$TL_Nu_1*$k1/$plate['De'];
+	$TL_T_1_w = 0.5*($T_2_b + $T_1_b);
+	$i = 0;
+
+
+	$start=$T_2_b;
+	$end = $T_1_b;
+	do {
+		//查询粘度
+		$sql3 = "SELECT * FROM `water_properties` WHERE `T`= '" . round($TL_T_1_w) . "'";
+		$rs3 = $db->query($sql3);
+		$row3 = $rs3->fetch();
+		$TL_miu_d_T_1_w = $row3[6];
+
+		$TL_Nu_1=$plate['TL_C_Nu']*exp($plate['TL_m_Nu']*log($TL_Re_1))
+			*exp(0.33*log($Pr1))*exp(0.14*log(($miu_d1/$TL_miu_d_T_1_w)));
+
+		
+		$TL_h_1=$TL_Nu_1*$k1/$plate['De'];
+		$TL_Q_1 = $TL_h_1 * ($T_1_b - $TL_T_1_w);
+		$TL_T_2_w = $TL_T_1_w - $TL_Q_1 * $plate['t_p']/$k_p;
+
+		$sql4 = "SELECT * FROM `water_properties` WHERE `T`= '" . round($TL_T_2_w) . "'";
+		$rs4 = $db->query($sql4);
+		$row4 = $rs4->fetch();
+		$TL_miu_d_T_2_w = $row4[6];
+
+		$TL_Nu_2=$plate['TL_C_Nu']*exp($plate['TL_m_Nu']*log($TL_Re_2))
+			*exp(0.4*log($Pr2))*exp(0.14*log(($miu_d2/$TL_miu_d_T_2_w)));
+		$TL_h_2=$TL_Nu_2*$k2/$plate['De'];
+		$TL_Q_2 = $TL_h_2 * ($TL_T_2_w - $T_2_b);
+
+
+		if ($TL_Q_1 >= $TL_Q_2) {
+			$start = $TL_T_1_w;
+		} else {
+			$end = $TL_T_1_w;
+		}
+		$TL_T_1_w = 0.5*($start + $end);
+
+		$i++;
+	} while (abs(($TL_Q_1-$TL_Q_2)/$TL_Q_1) > 0.001);
+
+
 	$TL_U=1/((1/$TL_h_2)+(1/$TL_h_1)+($plate['t_p']/$k_p));
 	$TL_NTU_1=2*$TL_U*$plate['A_r']/($TL_V_1_c*$roll1*$plate['A_c']*$C_p1*1000);
 	$TL_sigma_1=($TL_V_1_c*$roll1*$C_p1)/($TL_V_2_c*$roll2*$C_p2);
@@ -297,32 +361,15 @@ if ($W_1 >= $W_2) {
 	$TL_t_1=$TL_R_1*$Delta_ti;
 	$TL_W_1=$TL_V_1_c*$plate['A_c']*$roll1;
 }	
-/*
-echo "TL参数：<br >".
-"$roll2<br />".
-"$TL_V_2_c<br />".
-"$TL_V_1_c<br >".
-"$TL_Re_2<br >".
-"$TL_Re_1<br >".
-"$TL_Nu_2<br >".
-"$TL_Nu_1<br >";
-ec($TL_h_2);
-ec($TL_h_1);
-ec($TL_U);
-ec($TL_NTU_1);
-ec($TL_sigma_1);
-ec($TL_R_1);
-ec($TL_t_1);
-ec($TL_W_1);
-*/
+
+
+
+
 //计算整机R
 $Total_sigma=($W_1*$C_p1)/($W_2*$C_p2);
 $Total_R=((1-exp($NTU*(1-$Total_sigma))*log(exp(1))))/
 	(($Total_sigma-exp($NTU*(1-$Total_sigma))*log(exp(1))));
-/*
-echo "整机R：<br >";
-ec($Total_R);
-*/
+
 //计算温度
 $T_TK_1_b=$T_1_i-$TK_t_1/2;
 $T_TM_1_b=$T_1_i-$TM_t_1/2;
@@ -330,34 +377,74 @@ $T_TL_1_b=$T_1_i-$TL_t_1/2;
 $T_TK_2_b=$T_2_i+$TK_t_1/2*$TK_sigma_1;
 $T_TM_2_b=$T_2_i+$TM_t_1/2*$TM_sigma_1;
 $T_TL_2_b=$T_2_i+$TL_t_1/2*$TL_sigma_1;
-/*
-echo "实际各板定性温度：<br >";
-ec($T_TK_1_b);
-ec($T_TM_1_b);
-ec($T_TL_1_b);
-ec($T_TK_2_b);
-ec($T_TM_2_b);
-ec($T_TL_2_b);
-*/
+
 //计算板片数,M_为质量守恒，E_为能量守恒
+$M_TK_Ncp_double=$W_1/$TK_W_1;
+$M_TM_Ncp_double=$W_1/$TM_W_1;
+$M_TL_Ncp_double=$W_1/$TL_W_1;
+
 $M_TK_Ncp=ceil($W_1/$TK_W_1);
 $M_TM_Ncp=ceil($W_1/$TM_W_1);
 $M_TL_Ncp=ceil($W_1/$TL_W_1);
-/*
-echo "质量守恒下的TK、TM、TL的板片数:<br >";
-ec($M_TK_Ncp);
-ec($M_TM_Ncp);
-ec($M_TL_Ncp);
-*/
+
+
+$E_TK_Ncp_double=$W_1*$Delta_ti*$Total_R/($TK_W_1*$Delta_ti*$TK_R_1);
+$E_TM_Ncp_double=$W_1*$Delta_ti*$Total_R/($TM_W_1*$Delta_ti*$TM_R_1);
+$E_TL_Ncp_double=$W_1*$Delta_ti*$Total_R/($TL_W_1*$Delta_ti*$TL_R_1);
+
 $E_TK_Ncp=ceil($W_1*$Delta_ti*$Total_R/($TK_W_1*$Delta_ti*$TK_R_1));
 $E_TM_Ncp=ceil($W_1*$Delta_ti*$Total_R/($TM_W_1*$Delta_ti*$TM_R_1));
 $E_TL_Ncp=ceil($W_1*$Delta_ti*$Total_R/($TL_W_1*$Delta_ti*$TL_R_1));
-/*
-echo "能量守恒下的TK、TM、TL的板片数:<br >";
-ec($E_TK_Ncp);
-ec($E_TM_Ncp);
-ec($E_TL_Ncp);
-*/
+
+//混合TK、TM、TL计算板片数
+$TM_mixTMTL_Ncp_double = (($E_TL_Ncp_double-$M_TL_Ncp_double)/
+	(((-$M_TL_Ncp_double)/$M_TM_Ncp_double)-((-$E_TL_Ncp_double)/$E_TM_Ncp_double)));
+
+$TL_mixTMTL_Ncp_double = $TM_mixTMTL_Ncp_double*((-$M_TL_Ncp_double)/$M_TM_Ncp_double)+$M_TL_Ncp_double;
+
+$TM_mixTMTK_Ncp_double = ($E_TK_Ncp_double-$M_TK_Ncp_double)/
+	((-$M_TK_Ncp_double/$M_TM_Ncp_double)-(-$E_TK_Ncp_double/$E_TM_Ncp_double));
+
+$TK_mixTMTK_Ncp_double = $TM_mixTMTK_Ncp_double*(-$M_TK_Ncp_double/$M_TM_Ncp_double)+$M_TK_Ncp_double;
+
+$TM_mixTMTL_Ncp = ceil($TM_mixTMTL_Ncp_double);
+$TL_mixTMTL_Ncp = ceil($TL_mixTMTL_Ncp_double);
+$TM_mixTMTK_Ncp = ceil($TM_mixTMTK_Ncp_double);
+$TK_mixTMTK_Ncp = ceil($TK_mixTMTK_Ncp_double);
+//总板片数
+$sum_mixTMTL_Ncp = 2 * ($TM_mixTMTL_Ncp+$TL_mixTMTL_Ncp);
+$sum_mixTMTK_Ncp = 2 * ($TM_mixTMTK_Ncp+$TK_mixTMTK_Ncp);
+
+$sum_M_TK_Ncp = 2 * $M_TK_Ncp;
+$sum_M_TM_Ncp = 2 * $M_TM_Ncp;
+$sum_M_TL_Ncp = 2 * $M_TL_Ncp;
+
+$sum_E_TK_Ncp = 2 * $E_TK_Ncp;
+$sum_E_TM_Ncp = 2 * $E_TM_Ncp;
+$sum_E_TL_Ncp = 2 * $E_TL_Ncp;
+//换热面积
+$A_total_mixTMTL = $plate['A_r'] * $sum_mixTMTL_Ncp;
+$A_total_mixTMTK = $plate['A_r'] * $sum_mixTMTK_Ncp;
+
+$A_total_M_TK = $plate['A_r'] * $sum_M_TK_Ncp;
+$A_total_M_TM = $plate['A_r'] * $sum_M_TM_Ncp;
+$A_total_M_TL = $plate['A_r'] * $sum_M_TL_Ncp;
+
+$A_total_E_TK = $plate['A_r'] * $sum_E_TK_Ncp;
+$A_total_E_TM = $plate['A_r'] * $sum_E_TM_Ncp;
+$A_total_E_TL = $plate['A_r'] * $sum_E_TL_Ncp;
+//K值
+$U_mixTMTL = $Q/$LMTD/$A_total_mixTMTL*1000;
+$U_mixTMTK = $Q/$LMTD/$A_total_mixTMTK*1000;
+
+$U_M_TK = $Q/$LMTD/$A_total_M_TK*1000;
+$U_M_TM = $Q/$LMTD/$A_total_M_TM*1000;
+$U_M_TL = $Q/$LMTD/$A_total_M_TL*1000;
+
+$U_E_TK = $Q/$LMTD/$A_total_E_TK*1000;
+$U_E_TM = $Q/$LMTD/$A_total_E_TM*1000;
+$U_E_TL = $Q/$LMTD/$A_total_E_TL*1000;
+
 /*
 	待补充：
 	$miu_b = ;
@@ -694,46 +781,6 @@ if ((ΔT1'-ΔT1) < 1) {
 	</div>	
 	<hr >
 	<div class="panel panel-default">
-	  <div class="panel-heading">根据T_1_w和T_2_w的值查询粘度</div>
-	  <table class="table table-hover table-bordered">
-	  	<thead>
-	  		<tr>
-	  			<th width="5%">#</th>
-	  			<th width="40%">名称</th>
-	  			<th width="40%">数值</th>
-	  			<th width="15%">单位</th>
-	  		</tr>
-	  	</thead>
-	    <tbody>
-	    	<tr>
-	    		<th>1</th>
-	    		<th>T_1_w</th>
-	    		<th><?=$T_1_w?></th>
-	    		<th>°C</th>
-	    	</tr>
-	    	<tr>
-	    		<th>2</th>
-	    		<th>T_2_w</th>
-	    		<th><?=$T_2_w?></th>
-	    		<th>°C</th>
-	    	</tr>
-	    	<tr>
-	    		<th>3</th>
-	    		<th>miu_d_T_1_w</th>
-	    		<th><?=$miu_d_T_1_w?></th>
-	    		<th>°C</th>
-	    	</tr>
-	    	<tr>
-	    		<th>4</th>
-	    		<th>miu_d_T_2_w</th>
-	    		<th><?=$miu_d_T_2_w?></th>
-	    		<th>°C</th>
-	    	</tr>
-	    </tbody>
-	  </table>
-	</div>	
-	<hr >
-	<div class="panel panel-default">
 	  <div class="panel-heading">TK参数</div>
 	  <table class="table table-hover table-bordered">
 	  	<thead>
@@ -829,10 +876,35 @@ if ((ΔT1'-ΔT1) < 1) {
 	    		<th><?=$TK_W_1?></th>
 	    		<th>°C</th>
 	    	</tr>
+	    	<tr>
+	    		<th>15</th>
+	    		<th>TK_T_1_w</th>
+	    		<th><?=$TK_T_1_w?></th>
+	    		<th>°C</th>
+	    	</tr>
+	    	<tr>
+	    		<th>16</th>
+	    		<th>TK_T_2_w</th>
+	    		<th><?=$TK_T_2_w?></th>
+	    		<th>°C</th>
+	    	</tr>
+	    	<tr>
+	    		<th>17</th>
+	    		<th>TK_miu_d_T_1_w</th>
+	    		<th><?=$TK_miu_d_T_1_w?></th>
+	    		<th>°C</th>
+	    	</tr>
+	    	<tr>
+	    		<th>18</th>
+	    		<th>TK_miu_d_T_2_w</th>
+	    		<th><?=$TK_miu_d_T_2_w?></th>
+	    		<th>°C</th>
+	    	</tr>
 	    </tbody>
 	  </table>
 	</div>	
 	<hr >
+
 	<div class="panel panel-default">
 	  <div class="panel-heading">TM参数</div>
 	  <table class="table table-hover table-bordered">
@@ -929,10 +1001,35 @@ if ((ΔT1'-ΔT1) < 1) {
 	    		<th><?=$TM_W_1?></th>
 	    		<th>°C</th>
 	    	</tr>
+	    	<tr>
+	    		<th>15</th>
+	    		<th>TM_T_1_w</th>
+	    		<th><?=$TM_T_1_w?></th>
+	    		<th>°C</th>
+	    	</tr>
+	    	<tr>
+	    		<th>16</th>
+	    		<th>TM_T_2_w</th>
+	    		<th><?=$TM_T_2_w?></th>
+	    		<th>°C</th>
+	    	</tr>
+	    	<tr>
+	    		<th>17</th>
+	    		<th>TM_miu_d_T_1_w</th>
+	    		<th><?=$TM_miu_d_T_1_w?></th>
+	    		<th>°C</th>
+	    	</tr>
+	    	<tr>
+	    		<th>18</th>
+	    		<th>TM_miu_d_T_2_w</th>
+	    		<th><?=$TM_miu_d_T_2_w?></th>
+	    		<th>°C</th>
+	    	</tr>
 	    </tbody>
 	  </table>
 	</div>	
 	<hr >
+
 	<div class="panel panel-default">
 	  <div class="panel-heading">TL参数</div>
 	  <table class="table table-hover table-bordered">
@@ -1027,6 +1124,30 @@ if ((ΔT1'-ΔT1) < 1) {
 	    		<th>14</th>
 	    		<th>TL_W_1</th>
 	    		<th><?=$TL_W_1?></th>
+	    		<th>°C</th>
+	    	</tr>
+	    	<tr>
+	    		<th>15</th>
+	    		<th>TL_T_1_w</th>
+	    		<th><?=$TL_T_1_w?></th>
+	    		<th>°C</th>
+	    	</tr>
+	    	<tr>
+	    		<th>16</th>
+	    		<th>TL_T_2_w</th>
+	    		<th><?=$TL_T_2_w?></th>
+	    		<th>°C</th>
+	    	</tr>
+	    	<tr>
+	    		<th>17</th>
+	    		<th>TL_miu_d_T_1_w</th>
+	    		<th><?=$TL_miu_d_T_1_w?></th>
+	    		<th>°C</th>
+	    	</tr>
+	    	<tr>
+	    		<th>18</th>
+	    		<th>TL_miu_d_T_2_w</th>
+	    		<th><?=$TL_miu_d_T_2_w?></th>
 	    		<th>°C</th>
 	    	</tr>
 	    </tbody>
@@ -1163,8 +1284,62 @@ if ((ΔT1'-ΔT1) < 1) {
 	    	</tr>
 	    </tbody>
 	  </table>
+	</div>
+	<hr >	
+	<div class="panel panel-default">
+	  <div class="panel-heading">结果分析</div>
+	  <table class="table table-hover table-bordered">
+	  	<thead>
+	  		<tr>
+	  			<th width="20%">#</th>
+	  			<th width="10%">TMTL</th>
+	  			<th width="10%">TMTK</th>
+	  			<th width="10%">M_TK</th>
+	  			<th width="10%">M_TM</th>
+	  			<th width="10%">M_TL</th>
+	  			<th width="10%">E_TK</th>
+	  			<th width="10%">E_TM</th>
+	  			<th width="10%">E_TL</th>
+	  		</tr>
+	  	</thead>
+	    <tbody>
+	    	<tr>
+	    		<th>总板片数</th>
+	    		<th><?=$sum_mixTMTL_Ncp?></th>
+	    		<th><?=$sum_mixTMTK_Ncp?></th>
+	    		<th><?=$sum_M_TK_Ncp?></th>
+	    		<th><?=$sum_M_TM_Ncp?></th>
+	    		<th><?=$sum_M_TL_Ncp?></th>
+	    		<th><?=$sum_E_TK_Ncp?></th>
+	    		<th><?=$sum_E_TM_Ncp?></th>
+	    		<th><?=$sum_E_TL_Ncp?></th>
+	    	</tr>
+	    	<tr>
+	    		<th>换热面积</th>
+	    		<th><?=$A_total_mixTMTL?></th>
+	    		<th><?=$A_total_mixTMTK?></th>
+	    		<th><?=$A_total_M_TK?></th>
+	    		<th><?=$A_total_M_TM?></th>
+	    		<th><?=$A_total_M_TL?></th>
+	    		<th><?=$A_total_E_TK?></th>
+	    		<th><?=$A_total_E_TM?></th>
+	    		<th><?=$A_total_E_TL?></th>
+	    	</tr>
+	    	<tr>
+	    		<th>换热系数</th>
+	    		<th><?=$U_mixTMTL?></th>
+	    		<th><?=$U_mixTMTK?></th>
+	    		<th><?=$U_M_TK?></th>
+	    		<th><?=$U_M_TM?></th>
+	    		<th><?=$U_M_TL?></th>
+	    		<th><?=$U_E_TK?></th>
+	    		<th><?=$U_E_TM?></th>
+	    		<th><?=$U_E_TL?></th>
+	    	</tr>
+	    </tbody>
+	  </table>
 	</div>	
-
+	<hr >
 </div>
 
   </body>
